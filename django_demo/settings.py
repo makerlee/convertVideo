45f0b71af9f62eb6dc6e3a199a6260d1f625dc05
jiyang.li <jiyang.li@timanetworks.com>
2018-04-03 15:09:44 +0800

    一些优化

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b5ea%64)g(laepbxov4#^j0%q$and2-f01-xtq7jlq*%=8n3v7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
